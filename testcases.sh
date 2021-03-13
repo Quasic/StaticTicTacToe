551e94ec16fa470d9a4ecfbbf9d396b52256c845
Quasic <qbasic010@aim.com>
2021-03-13 15:18:52 -0500

    testcases.sh: ready

--- testcases.sh
+++ testcases.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")/tictactoe"&&
-bash testcases.sh&&
-cd c&&
-true
-#bash testcases.sh
+x=0
+if cd "$(dirname "${BASH_SOURCE[0]}")/tictactoe"
+then printf 'Testing you moving first...\n'
+	bash testcases.sh
+	r=$?
+	if cd c
+	then printf 'Testing me moving first...\n'
+		bash testcases.sh
+		r=$((r+$?))
+	else r=$((r+1))
+		x=1
+	fi
+else r=1
+	x=1
+fi
+printf '%i errors total\n' "$r"
+if [ "$x" = 1 ]
+then r=255
+else [ 254 -lt "$r" ]&&r=254
+fi
+[[ $- = *i* ]]&&read -rn1 -p "Press a key to return $r..."
+exit "$r"
